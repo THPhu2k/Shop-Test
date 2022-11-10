ddff70ff9ec05ef8b7174611880fe82b7b0f2626
THPhu2k <tranhoangphu10022006@gmail.com>
2022-11-10 13:29:26 +0700

    New Commit

---
+++ Netflix/index.js
@@ -0,0 +1,35 @@
+const netflix = async() => {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'b6e5c43dcamsh0acfaea2676ac9fp11af8djsn7de1bffb9d03',
+			'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
+		}
+	};
+	
+	var response = ''
+    var filmCount = 0
+	response = await fetch('https://unogs-unogs-v1.p.rapidapi.com/search/titles?order_by=date&type=movie', options)
+
+	data = await response.json()
+
+	console.log(data);
+
+	film = data.results
+	film.forEach(element => {
+		console.log(element);
+		var imgLink = element.img
+
+		img = document.createElement('img')
+
+		img.src = imgLink
+
+		if(filmCount >= 0 & filmCount < 5){
+			document.getElementById('row-poster1').appendChild(img)
+       	    filmCount ++
+            console.log(filmCount)
+		}
+	});
+
+}
+netflix();
